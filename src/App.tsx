6af6b38c0900556b98a182c09f628a0375b14d5a
Divyansh Raj <rajdivyansh2000@gmail.com>
2024-08-05 02:33:50 +0530

    notes and tags work correctly now

--- src/App.tsx
+++ src/App.tsx
@@ -53,12 +53,25 @@ function App() {
     });
   }
 
+  function addTag(tag: Tag) {
+    setTags((prev) => [...prev, tag]);
+  }
+
   return (
     <Container className="my-4">
       <Routes>
         {/* <Route exact path="/" element={<>Home</>} /> */}
         <Route path="/" element={<>Home</>} />
-        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
